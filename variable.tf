e44f136875a0048c68aace9285fdb337dec9cff5
AssianAk <107809552+AssianAk@users.noreply.github.com>
2025-10-30 00:46:44 +0530

    Add variables for AMI, instance type, and key name

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+    description = "callling ami"
+    type = string
+    default = ""
+}
+
+variable "instance_type" {
+    description = "instance type"
+    type = string
+    default = ""
+}
+
+variable "key_name" {
+    description = "caling key"
+    type = string
+    default = ""
+}
